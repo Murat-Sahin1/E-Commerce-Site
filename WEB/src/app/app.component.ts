8c29b503197c08b2d6e75376224aee9907544612
Voul <18155057@mersin.edu.tr>
2022-07-29 15:23:17 +0300

    Anguler set up to HTTPS.

--- WEB/src/app/app.component.ts
+++ WEB/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
-@Component({
+@Component({ //Decorator
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html', //can access the properties of this component inside the templateUrl
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'WEB';
+  title = 'E-Commerce';
 }
